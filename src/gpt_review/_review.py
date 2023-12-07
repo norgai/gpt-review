7c0818a3994b0fb59c607514b173540e2222b237
DaejungLee <daejunglee@kr.kpmg.com>
2023-12-07 16:46:47 +0900

    Update _review.py

--- src/gpt_review/_review.py
+++ src/gpt_review/_review.py
@@ -109,7 +109,7 @@ def _summarize_pr(git_diff) -> str:
     text = ""
     if os.getenv("FULL_SUMMARY", "true").lower() == "true":
         text += f"""
-{_request_goal(git_diff, goal="Below is a code patch, please help me do a brief code review on it.Answer me in Korean.")}
+{_request_goal(git_diff, goal="Below is a code patch, please help me do a brief code review on it very simply.Answer me in Korean.")}
 """
 
         text += _check_goals(git_diff, _CHECKS["SUMMARY_CHECKS"])
