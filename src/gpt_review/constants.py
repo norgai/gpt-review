41356752130ad578b433cd0f30e20b84a14adabb
DaejungLee <daejunglee@kr.kpmg.com>
2023-12-26 16:05:56 +0900

    add large model

--- src/gpt_review/constants.py
+++ src/gpt_review/constants.py
@@ -30,7 +30,7 @@ AZURE_API_VERSION = "2023-07-01-preview"
 AZURE_CONFIG_FILE = "azure.yaml"
 AZURE_TURBO_MODEL = "gpt-4"
 AZURE_SMART_MODEL = "gpt-4"
-AZURE_LARGE_MODEL = "gpt-4"
+AZURE_LARGE_MODEL = "gpt-4-32k"
 AZURE_EMBEDDING_MODEL = "text-embedding-ada-002"
 AZURE_KEY_VAULT = "https://dciborow-openai.vault.azure.net/"
 
